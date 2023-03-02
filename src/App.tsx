e02e8572c69372df8e02d85279429313400386aa
VoskanianGor <vosckaniangor@gmail.com>
2023-03-02 17:50:08 +0300

    Add API  request and add types for response

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import calls from './mock.json'
+
+const startDate = '2022-01-01'
+const endDate = '2022-12-31'
+
+const getCalls = async () => {
+	const res = await fetch(
+		`https://api.skilla.ru/mango/getList?date_start=${startDate}&date_end=${endDate}&in_out=`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer testtoken',
+			},
+		}
+	)
+
+	return await res.json()
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+	// const [calls, setCalls] = useState([])
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	// useEffect(() => {
+	// 	getCalls().then(setCalls)
+	// }, [])
+
+	return <div className="App">{JSON.stringify(calls)}</div>
 }
 
 export default App
---
+++ src/types/calls.ts
@@ -0,0 +1,75 @@
+export interface GetCallListResponse {
+	total_rows: string
+	results: Call[]
+}
+
+export interface Call {
+	id: number
+	partnership_id: string
+	partner_data: PartnerData
+	date: string
+	date_notime: string
+	time: number
+	from_number: string
+	from_extension: string
+	to_number: string
+	to_extension: string
+	is_skilla: number
+	status: string
+	record: string
+	line_number: string
+	in_out: number
+	from_site: number
+	source: string
+	errors: Error[]
+	disconnect_reason: string
+	results: Result[]
+	stages: Stage[]
+	abuse: Abuse[]
+	contact_name: string
+	contact_company: string
+	person_id: number
+	person_name: string
+	person_surname: string
+	person_avatar: string
+}
+
+export interface PartnerData {
+	id: string
+	name: string
+	phone: string
+}
+
+export interface Error {
+	title: string
+}
+
+export interface Result {
+	type: string
+	title: string
+	tooltip: string
+}
+
+export interface Stage {
+	person_name: string
+	person_surname: string
+	person_mango_phone: string
+	duration: string
+	disconnect_reason: string
+}
+
+export interface Abuse {
+	date: string
+	person_name: string
+	message: string
+	support_read_status: number
+	support_answer_status: number
+	answers: Answer[]
+}
+
+export interface Answer {
+	message: string
+	from_support: number
+	support_read_status: number
+	person_read_status: number
+}
